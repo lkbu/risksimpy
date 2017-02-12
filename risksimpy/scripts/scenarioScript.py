7c65e02891dfd2b88fd6dbe01a1e32b8a0bc849d
lkbu <lkbu@outlook.com>
2017-02-12 20:31:38 +0800

    scenario script

---
+++ risksimpy/scripts/scenarioScript.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 2017
+@desc: scenario usage
+@author: lkbu
+"""
+import sys
+import methods
+import pandas as pd
+import numpy as np
+import os
+import datetime as dt
